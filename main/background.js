e18660dfa90aaf552c777980b8a2f66fa64d0401
Vitalii Malchenko <vmalchenko7@gmail.com>
2023-10-09 16:36:09 +0300

    update/8
    update/8
    
    Co-Authored-By: Dream is not only dream. <145146800+dreamerstar123@users.noreply.github.com>
    Co-Authored-By: TinkerBobbleDev <145171754+tinkerbobbledev@users.noreply.github.com>
    Co-Authored-By: ballanse <145154449+ballanse@users.noreply.github.com>
    Co-Authored-By: Dandy <146976224+dandytopstar@users.noreply.github.com>

--- main/background.js
+++ main/background.js
@@ -32,7 +32,6 @@ if (isProd) {
   } else {
     const port = process.argv[2];
     await mainWindow.loadURL(`http://localhost:${port}/home`);
-    // mainWindow.webContents.openDevTools();
   }
 
 })();
